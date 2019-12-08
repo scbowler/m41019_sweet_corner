18175ca970ae687fb73840c2dc7c1fc5bd54c221
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-08 01:17:43 +0000

    Setup client and created images endpoint

---
+++ routes/images/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
+// Routes /images
+
+// GET /images/:type/:file
+router.get('/:type/:file', (req, res) => {
+    const { type, file } = req.params;
+
+    const imagePath = path.resolve(__dirname, type, file);
+
+    if(fs.existsSync(imagePath)) {
+        res.sendFile(imagePath);
+        return;
+    }
+
+    res.status(404).send('Not Found');
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const apiRouter = require('./api');
+const imagesRouter = require('./images');
 
 // Routes For /
 
 // ALL METHODS /api
 router.use('/api', apiRouter);
+
+// ALL METHODS /images
+router.use('/images', imagesRouter);
 
 module.exports = router;
